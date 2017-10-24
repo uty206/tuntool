1b54a0e5ee2a005c9699c0340f5530edd86e87f7
ruaron <ruaron@klass1-23-l.intra.itcollege.ee>
2017-10-24 12:35:49 +0300

    Fixed typos

--- tuntool.py
+++ tuntool.py
@@ -163,7 +163,7 @@ def main(args):
     except KeyboardInterrupt:
         print('\nOperation interrupted.')
 
-# Executing the main script and communism enemies.
+# Executing the main script and enemies of communism.
 if __name__ == '__main__':
     main(sys.argv)
 
